2633ee2c6b0cbd56ae6b3c1dd75fa1f910ca629b
Guillaume Hullin <guillaume.hullin@futur-tech.com>
2021-02-19 15:13:56 +0100

    commit

--- deploy.sh
+++ deploy.sh
@@ -11,12 +11,12 @@ source "$(dirname "$0")/ft-util/ft-util_inc_var"
 
 $S_LOG -d $S_NAME "Start $S_NAME $*"
 
-
-if [ "$1" = "agent" ] or [ "$1" = "proxy" ]
+if [ "$1" = "agent" ] || [ "$1" = "proxy" ]
 then
-    $ZBX_TYPE="$1" # "agent" or "proxy"
+    ZBX_TYPE="$1" # "agent" or "proxy"
 else
     $S_LOG -d $S_NAME "Doing nothing, you need to give at least one parameter (agent or proxy)."
+    $S_LOG -d "$S_NAME" "End $S_NAME"
     exit 0
 fi
 $S_LOG -d "$S_NAME" "The script will run for Zabbix $ZBX_TYPE" 
@@ -27,10 +27,10 @@ $S_LOG -d "$S_NAME" "The script will run for Zabbix $ZBX_TYPE"
 #############################
 #############################
 
-if [ -d "/etc/zabbix/" ]
+if [ -d "/etc/zabbix" ]
 then
     OS="Linux"
-    ZBX_ETC="/etc/zabbix/"
+    ZBX_ETC="/etc/zabbix"
     PSK_FLD="/home/zabbix"
     
 elif [ -d "/usr/local/zabbix/etc/" ]
@@ -84,14 +84,16 @@ esac
 #############################
 #############################
 
-mkdir ${PSK_FLD}/
-chown zabbix:zabbix ${PSK_FLD}
-# chmod 700 ${PSK_FLD}
-
+if [ ! -d "${PSK_FLD}" ]
+then
+    mkdir ${PSK_FLD}/
+    chown zabbix:zabbix ${PSK_FLD}
+    # chmod 700 ${PSK_FLD}
+fi
 
 echo $PSK_KEY > ${PSK_FLD}/key.psk
 chown zabbix:zabbix ${PSK_FLD}/key.psk
-chmod 600 ${PSK_FLD}/secret.psk
+chmod 600 ${PSK_FLD}/key.psk
 
 ZBX_PSK_CONF=${ZBX_ETC}/zabbix_${ZBX_TYPE}d.d/ft-psk.conf
 
@@ -100,7 +102,8 @@ echo "TLSConnect=psk" > $ZBX_PSK_CONF
 echo "TLSAccept=psk" >> $ZBX_PSK_CONF
 echo "TLSPSKFile=${PSK_FLD}/key.psk" >> $ZBX_PSK_CONF
 echo "TLSPSKIdentity=$PSK_IDENTITY" >> $ZBX_PSK_CONF
-echo "UserParameter=ft-psk.identity, grep TLSPSKIdentity= ${ZBX_PSK_CONF}" >> $ZBX_PSK_CONF
+echo "UserParameter=ft-psk.identity, grep -oP '^TLSPSKIdentity=\K.+' ${ZBX_PSK_CONF}" >> $ZBX_PSK_CONF
+echo "UserParameter=ft-psk.key.lastmodified, stat --format=%Y ${PSK_FLD}/key.psk" >> $ZBX_PSK_CONF
 
 $S_LOG -d "$S_NAME" -d "New PSK installed."
 
@@ -109,9 +112,9 @@ echo "####################################################"
 echo "####################################################"
 echo "Update your server-side host configuration!"
 echo "PSK_IDENTITY="
-echo $PSK_IDENTITY
+grep -oP '^TLSPSKIdentity=\K.+' ${ZBX_PSK_CONF}
 echo "PSK_KEY="
-echo $PSK_KEY
+cat ${PSK_FLD}/key.psk
 echo "####################################################"
 echo "####################################################"
 echo "####################################################"
